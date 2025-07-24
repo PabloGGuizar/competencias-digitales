e11e50797c06e88cf68b3b0bb74f87f593ca5104
Pablo G. Guízar <82488599+PabloGGuizar@users.noreply.github.com>
2025-07-23 18:41:29 -0600

    Fix: Usar carpeta public para el logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 // src/App.jsx
 
 import React from 'react';
-// --- 1. IMPORTA LA IMAGEN AQUÍ ---
-import mapaMentalLogo from './assets/mapa-mental.png'; 
 import { MapaConceptual } from './components/MapaConceptual';
-import './App.css';
+import './App.css'; // Importamos un nuevo archivo de estilos para la App
 
 function App() {
   return (
     <div className="app-container">
       <header className="app-header">
-        {/* --- 2. USA LA VARIABLE IMPORTADA EN EL SRC --- */}
-        <h1 className="titulo-principal">
-          <img width="40" src={mapaMentalLogo} alt="logo" />
-          <span>Marco de Competencia Digital</span>
+        <h1 class="titulo-principal">
+          <img width="40" src="/mapa-mental.png" alt="logo" />
+          <span>Marco de Competencias Digitales</span>
         </h1>
-        <p>Haz clic en las competencias para ver más detalles</p>
+        <p>👇 Haz clic en las competencias para ver más detalles</p>
       </header>
       <MapaConceptual />
     </div>
